69106ea59f5b82952c8796478554966c77686188
Johnny Five <johnnyfive@johnny-five.local>
2017-10-30 15:14:46 -0500

    C# Fund 1

---
+++ fundamentals1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace fundamentals1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 255; i++){
+                Console.WriteLine(i);
+            }
+
+            for(int i = 1; i <= 100; i++){
+                if(i%5==0 && i%3==0){
+                    Console.WriteLine("FizzBuzz");
+                }
+                if(i%5==0){
+                    Console.WriteLine("Buzz");
+                }
+                if(i%3==0){
+                    Console.WriteLine("Fizz");
+                }
+
+            }
+        }
+    }
+}
